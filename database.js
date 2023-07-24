a848775d8e881ff30b3c9d97c3815e1b290337e5
maxwellwebb <maxwellsdwebb@gmail.com>
2023-07-24 12:45:59 -0700

    Added external links to matches page

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const TABLE_URL = "https://api.airtable.com/v0/appo1y5AaYd3oeCyi/Colleges"; //Defining the link to the database we have setup
 const KEY_QUERY = "api_key=keyDhRLs1MlrCykL7"; //API key
-const SUMMARY_QUERY = "fields%5B%5D=Picture&fields%5B%5D=Name&fields%5B%5D=Location&fields%5B%5D=Average%20GPA&fields%5B%5D=Average%20SAT&fields%5B%5D=Campus%20setting&fields%5B%5D=Acceptance%20Rate&fields%5B%5D=Size"; //Defining and linking all of the collumns from our database
+const SUMMARY_QUERY = "fields%5B%5D=Picture&fields%5B%5D=Name&fields%5B%5D=Location&fields%5B%5D=Average%20GPA&fields%5B%5D=Average%20SAT&fields%5B%5D=Campus%20setting&fields%5B%5D=Acceptance%20Rate&fields%5B%5D=Size&fields%5B%5D=Tuition"; //Defining and linking all of the collumns from our database
 var itemList = [];
 
 function fetchItemsPage(itemResultElement, offset) {
@@ -11,9 +11,11 @@ function fetchItemsPage(itemResultElement, offset) {
 
             var matchedColleges = ""; //Makes this variable have no words
             var matchedColleges2 = "No colleges match your search";
-
+            var arr = new Array();
+            
             for (var p = 0; p < data.records.length; p++) { //Goes through every value in the database
                 var pictureLink = data.records[p].fields["Picture"];
+                var Tuition = data.records[p].fields["Tuition"];
                 var Name = data.records[p].fields["Name"]; //All of these are just defining collumn from our database as variables
                 var Location = data.records[p].fields["Location"];
                 var GPA = (data.records[p].fields["Average GPA"]);
@@ -40,17 +42,26 @@ function fetchItemsPage(itemResultElement, offset) {
                 var selectElement6 = document.getElementById("myRange3");
                 var selectedValue6 = (selectElement6.value);
 
+                var selectElement7 = document.getElementById("myRange4");
+                var selectedValue7 = (selectElement7.value);
+
+
                 if((selectedValue == "N/A" || Location == selectedValue) &&
                     (Size == selectedValue2 || selectedValue2 == "N/A") &&
                     (Setting == selectedValue3|| selectedValue3 == "N/A") &&
-                    Acceptance > selectedValue4 &&
-                    GPA <= selectedValue5 &&
-                    SAT <= selectedValue6) { //Checks every single collumn for the users selected value ie. if the user selects medium size schools it checks for all medium size schools
-                    console.log(data.records[p].fields["Name"]); //Logs all of the names of the schools that matches the users preferences
-                    matchedColleges += `<a href="template.html?college=${encodeURIComponent([p])}">${data.records[p].fields["Name"]}</a><br>`; // Add the users matched college names to the string                
-                    matchedColleges2 = "";
+                    (Acceptance > selectedValue4) &&
+                    (GPA <= selectedValue5) &&
+                    (SAT <= selectedValue6) &&
+                    (Tuition <= selectedValue7))
+                    { //Checks every single collumn for the users selected value ie. if the user selects medium size schools it checks for all medium size schools
+                        arr.push(data.records[p].fields["Name"]);
+                        console.log(arr);
                 }
+                
             }
+            
+
+            sessionStorage.setItem("arr", JSON.stringify(arr));
             
             document.getElementById("Match").innerHTML = matchedColleges; // Display the list of matched colleges
             document.getElementById("Match2").innerHTML = matchedColleges2;
